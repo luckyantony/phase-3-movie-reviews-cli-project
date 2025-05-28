b0d35d9996dc828b763adf2da0225d472e9922e2
luckyantony <luckyantonyofficial@gmail.com>
2025-05-29 00:39:55 +0300

    Made some changes

--- debug.py
+++ debug.py
@@ -2,27 +2,22 @@ from models.reviewers import Reviewer
 from models.movie import Movie
 from models.review import Review
 
-# Try creating a reviewer
 r = Reviewer("Test Reviewer")
 r.save()
 print("Created reviewer:", r)
 
-# Try creating a movie
 m = Movie("The Matrix", "Sci-Fi")
 m.save()
 print("Created movie:", m)
 
-# Try creating a review
 rev = Review(r.id, m.id, 5, "Mind-blowing effects!")
 rev.save()
 print("Created review:", rev)
 
-# Print all movies
 print("All movies:")
 for movie in Movie.all():
     print(movie)
 
-# Print all reviews
 print("All reviews:")
 for review in Review.all():
     print(review)
