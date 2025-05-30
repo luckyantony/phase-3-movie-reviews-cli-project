60da99e005c61d4839a7b91d42f6155cc04b0d28
luckyantony <luckyantonyofficial@gmail.com>
2025-05-30 15:31:58 +0300

    Updating the seeding file

--- scripts/run_seed.py
+++ scripts/run_seed.py
@@ -1,2 +1,6 @@
+# In scripts/run_seed.py
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from seed import *
 print("Database seeded with initial data.")
